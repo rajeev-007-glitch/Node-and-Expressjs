50087853fb4e0177b216c5c0889479da7ffe8498
Rajeev Singh Jadon <jadonrajeevsingh237@gmail.com>
2022-09-17 20:31:18 +0530

    Adding new files...

--- 04-store-api/app.js
+++ 04-store-api/app.js
@@ -1,13 +1,42 @@
 require('dotenv').config()
-//* async errors
+require('express-async-errors')
 
 const express = require('express')
 const app = express()
 
+const connectDB = require('./db/connect')
+const router = require('./routes/products')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+const { application } = require('express')
+
+//*Middleware
+app.use(express.json())
+
+
+//*routes
+
 app.get('/', (req, res)=>{
-    res.send("Yes App can now get '/' ")
+    res.send('<h1>Store API</h1> <a href="/api/v1/products">Product route</a>')
 })
 
-app.listen(5000, ()=>{
-    console.log('App is listning on port 5000...')
-})
+app.use('/api/v1/products', router)
+
+//*products routes
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 5000
+      
+const start = async()=>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`App is listning on port ${port}...`))
+    } catch (error) {
+        console.log(error)              
+    }
+}
+
+start()
